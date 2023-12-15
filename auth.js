10033beb19160152e7be6bb002d886d8d4634f2d
jbaileymusic <46546066+jbaileymusic@users.noreply.github.com>
2023-12-14 22:16:47 -0500

    update errors

--- auth.js
+++ auth.js
@@ -22,7 +22,9 @@ module.exports = (router) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right...',
-                    user: user
+                    user: user,
+                    error,
+                    info
                 });
             }
             req.login(user, { session: false }, (error) => {
